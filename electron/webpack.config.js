2c5a89b7cbe24405f7d2ff784ea67aee931e7cdc
Masami Ogoshi <ogochan@wasp.co.jp>
2023-04-04 07:46:06 +0000

    forgot

---
+++ electron/webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const MODE = "development";
+//const MODE = "production";
+
+const prod = MODE === 'production';
+
+module.exports = {
+	mode: MODE,
+	context: path.resolve(__dirname, '.'),
+    entry: {
+		renderer: './renderer.js',
+		electron: './electron.js'
+	},
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        //publicPath: '/manage/dist/',
+        filename: '[name].js'
+    },
+    devtool: false,
+	watchOptions: {
+		ignored: [ 'node_modules', 'public', 'dist', 'config', 'client', 'release' ]
+	},
+	module: {
+        rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						compilerOptions: {
+							dev: !prod
+						},
+						emitCss: prod,
+						hotReload: !prod
+					}
+				}
+			},
+			{
+				test: /\.css/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{	//	https://stackoverflow.com/questions/68468150/webpack-custom-font-loading-resulted-ots-parsing-error-invalid-sfntversion
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9])?$/,
+				type: 'asset/resource',
+    			dependency: { not: ['url'] }
+			},
+            {
+				test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [
+                  {loader: 'file-loader'},
+                  {
+                    loader: 'svgo-loader',
+                    /*options: {
+                      plugins: [
+                        {removeXMLNS: true},
+                        {removeOffCanvasPaths: true},
+                        {removeDimensions: true},
+                        {reusePaths: true}
+                      ]
+                    }*/
+                  }
+				]
+			},
+			{
+                test: /\.(ttf|eot|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader',
+                }]
+            }
+		]
+    },
+	resolve: {
+		alias: {
+			path: "path-browserify",
+		},
+		modules: [
+			'node_modules',
+			'front/javascripts',
+			'libs'
+		],
+	}
+}
