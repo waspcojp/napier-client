09b8a69e6456270b84af4f9564d843328e8e8673
Masami Ogoshi <ogochan@wasp.co.jp>
2023-08-07 07:36:16 +0000

    authentication

---
+++ electron/front/svelte/web-server/password_list.svelte
@@ -0,0 +1,154 @@
+<div class="card">
+    <div class="card-header">
+        利用者一覧
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="col-2">
+                        ユーザ名
+                    </th>
+                    <th class="col-7">
+                        説明
+                    </th>
+                    <th class="col-1">
+                        状態
+                    </th>
+                    <th class="col-2">
+
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {#if (password)}
+                {#each password as pass,index}
+                <tr>
+                    <td>
+                        <a
+                            data-index={index}
+                            on:click|preventDefault={editUser}>
+                            {pass.name}
+                        </a>
+                    </td>
+                    <td>
+                        {pass.description || ''}
+                    </td>
+                    <td style="text-align:center;">
+                        {#if pass.expire}
+                        <div style="font-size:20px;" class="text-danger">
+                            <i title="利用停止中" class="fa-solid fa-ban"></i>
+                        </div>
+                        {/if}
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-danger btn-sm"
+							data-index={index}
+							on:click={Delete}>
+                            <i title="削除" class="fa-solid fa-trash-can"></i>
+                        </button>
+                        {#if !pass.expire}
+                        <button type="button" class="btn btn-warning btn-sm"
+							data-index={index}
+							on:click={Stop}>
+                            <i title="利用停止" class="fa-solid fa-ban"></i>
+                        </button>
+                        {/if}
+                        {#if pass.expire}
+                        <button type="button" class="btn btn-warning btn-sm"
+							data-index={index}
+							on:click={Restart}>
+                            <i title="利用再開" class="fa-solid fa-trash-arrow-up"></i>
+                        </button>
+                        {/if}
+                    </td>
+                </tr>
+                {/each}
+                {/if}
+            </tbody>
+        </table>
+    </div>
+    <div class="card-footer">
+        <button type="button" class="btn btn-primary"
+            on:click={newUser}>
+            新規登録
+        </button>
+    </div>
+</div>
+<PasswordPopup
+    on:update={update}
+    bind:entry={entry}
+    {modal}>
+</PasswordPopup>
+
+<script>
+import {onMount, beforeUpdate, afterUpdate, createEventDispatcher} from 'svelte';
+import Modal from 'bootstrap/js/dist/modal';
+
+import PasswordPopup from './password_popup.svelte';
+let modal;
+let entry;
+export let password;
+const dispatch = createEventDispatcher();
+
+let line_no;
+const editUser = (event) => {
+    line_no = event.target.dataset.index;
+    //console.log('line_no', line_no);
+    entry = {
+        name: password[line_no].name,
+        description: password[line_no].description
+    };
+    modal.show();
+}
+const Delete= (event) => {
+    line_no = event.currentTarget.dataset.index;
+    //console.log('line_no', line_no);
+    password.splice(line_no, 1);
+    password = password;
+    console.log(password);
+    dispatch('update');
+}
+const Stop = (event) => {
+    line_no = event.currentTarget.dataset.index;
+    //console.log('line_no', line_no);
+    password[line_no].expire = new Date();
+    dispatch('update');
+}
+const Restart = (event) => {
+    line_no = event.currentTarget.dataset.index;
+    //console.log('line_no', line_no);
+    password[line_no].expire = undefined;
+    dispatch('update');
+}
+const newUser = (event) => {
+    entry = {};
+    line_no = undefined;
+    modal.show();
+}
+const update = () => {
+    console.log({line_no});
+    if  ( line_no ) {
+        password[line_no].name = entry.name;
+        password[line_no].description = entry.description;
+        if  ( entry.password )  {
+            password[line_no].hash_password = undefined;
+            password[line_no].password = entry.password;
+        }
+    } else {
+        password.push({
+            name: entry.name,
+            description: entry.description,
+            password: entry.password
+        });
+    }
+    password = password;
+    dispatch('update');
+}
+afterUpdate(() => {
+    if  ( !modal )  {
+        modal = new Modal(document.getElementById('password-modal'));
+    }
+})
+
+</script>
---
+++ electron/front/svelte/web-server/password_popup.svelte
@@ -0,0 +1,73 @@
+<div class="modal" id="password-modal" tabindex="-1" data-bs-backdrop="static">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header ">
+		        <h5 class="modal-title" id="passwordLabel">利用者情報</h5>
+		        <button type="button" class="btn-close" id="password-close-button" area-label="Close"
+			        on:click={close_}></button>
+	        </div>
+            <form>
+                {#if entry }
+    	        <div class="modal-body">
+                    <div class="row">
+                        <label for="user_name" class="col-form-label">
+                            ユーザ名
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <input type="text" bind:value={entry.name} class="form-control" placeholder="User name">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="password" class="col-form-label">
+                            パスワード
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <input type="text" bind:value={entry.password} class="form-control" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <label for="description" class="col-form-label">
+                            説明
+                        </label>
+                    </div>
+                    <div class="row">
+                        <div class="input-group">
+                            <input type="text" bind:value={entry.description} class="form-control" placeholder="Description">
+                        </div>
+                    </div>
+                </div>
+                {/if}
+	            <div class="modal-footer">
+                    <button type="button" class="btn btn-info"
+                        on:click={close_}>
+                        取消
+                    </button>
+                    <button type="button" class="btn btn-primary"
+                        on:click={update}>
+                        登録
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+import {onMount, beforeUpdate, afterUpdate, createEventDispatcher} from 'svelte';
+const dispatch = createEventDispatcher();
+
+export let modal;
+export let entry;
+
+const close_ = () => {
+    console.log('close_');
+	modal.hide();
+}
+const update = () => {
+    dispatch('update');
+	modal.hide();
+}
+</script>
